0d200cc7aa5a13cf977193dc01fdeadd6cc076c2
Rndraha21 <robinndraha111@gmail.com>
2025-04-15 18:23:24 +0700

    update

--- api/index.ts
+++ api/index.ts
@@ -1,6 +1,6 @@
 import type { IncomingMessage, ServerResponse } from "http";
 
-const { handler } = require("../../build/server/index.js");
+const { handler } = require("../../build/server/nodejs_eyJydW50aW1lIjoibm9kZWpzIn0/index.js");
 
 export default function (req: IncomingMessage, res: ServerResponse) {
   // Use the handler exported by react-router-serve
