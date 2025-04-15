0c7a77671e8843eca5e02e1bcf8bc22dbbd06738
Rndraha21 <robinndraha111@gmail.com>
2025-04-15 18:25:14 +0700

    update

--- api/index.ts
+++ api/index.ts
@@ -2,7 +2,12 @@ import type { IncomingMessage, ServerResponse } from "http";
 
 const { handler } = require("../../build/server/nodejs_eyJydW50aW1lIjoibm9kZWpzIn0/index.js");
 
-export default function (req: IncomingMessage, res: ServerResponse) {
-  // Use the handler exported by react-router-serve
-  handler(req, res);
+export default async function (req: IncomingMessage, res: ServerResponse) {
+  try {
+    await handler(req, res);
+  } catch (error) {
+    console.error("SSR handler error:", error);
+    res.statusCode = 500;
+    res.end("Internal Server Error");
+  }
 }
