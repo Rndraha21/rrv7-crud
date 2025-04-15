32c6c16d8c413a7a3fedbca19e7f91e80d6aede6
Rndraha21 <robinndraha111@gmail.com>
2025-04-15 18:27:45 +0700

    update

--- api/index.ts
+++ api/index.ts
@@ -1,13 +1,6 @@
 import type { IncomingMessage, ServerResponse } from "http";
 
-const { handler } = require("../../build/server/nodejs_eyJydW50aW1lIjoibm9kZWpzIn0/index.js");
-
-export default async function (req: IncomingMessage, res: ServerResponse) {
-  try {
-    await handler(req, res);
-  } catch (error) {
-    console.error("SSR handler error:", error);
-    res.statusCode = 500;
-    res.end("Internal Server Error");
-  }
+export default function (req: IncomingMessage, res: ServerResponse) {
+  res.statusCode = 200;
+  res.end("Hello from test serverless function");
 }
